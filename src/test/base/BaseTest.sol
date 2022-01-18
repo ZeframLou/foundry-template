579b2b0a752dbf461f5a9932bbb836de349faf3b
ZeframLou <zeframlou@gmail.com>
2022-01-17 20:14:48 -0800

    fix: set BaseTest::vm to internal

--- src/test/base/BaseTest.sol
+++ src/test/base/BaseTest.sol
@@ -7,5 +7,5 @@ import {VM} from "../utils/VM.sol";
 import {console} from "../utils/console.sol";
 
 contract BaseTest is DSTest {
-    VM constant vm = VM(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    VM internal constant vm = VM(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 }
